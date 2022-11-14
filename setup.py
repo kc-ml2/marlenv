69b940defca7a8f34dca4471ed26459ebdab6a03
Anthony W. Jung <36752646+anthony0727@users.noreply.github.com>
2022-11-14 14:50:09 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 from setuptools import setup, find_packages
+import io
 
+with open("README.md") as f:
+    with io.open("README.md", encoding="utf-8") as f:
+        long_description = f.read()
+        
 setup(
     name='marlenv',
     version='1.0.0',
     url='https://github.com/kc-ml2/marlenv',
     author='Tae Min Ha, Daniel Nam, Won Seok Jung',
     author_email='contact@kc-ml2.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=[
         'gym==0.24.1',
     ],
     python_requires='>=3.8',
-    long_description_content_type="text/markdown",
+
 )
